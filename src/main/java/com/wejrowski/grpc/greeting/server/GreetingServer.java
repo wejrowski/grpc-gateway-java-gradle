5ec2273cc498314e086c382d9ebc86637a26266f
Brent Wejrowski <wejrowski@gmail.com>
2018-06-04 14:06:11 -0700

    Setup grpc java server

---
+++ src/main/java/com/wejrowski/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,26 @@
+package com.wejrowski.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+  public static void main(String[] args) throws IOException, InterruptedException {
+    System.out.println("Hello gRPC");
+
+    Server server = ServerBuilder.forPort(50051)
+        .build();
+
+    server.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      System.out.println("Received Shutdown Request");
+      server.shutdown();
+      System.out.println("Successfully stopped server");
+    }));
+
+    // Block main thread until terminated
+    server.awaitTermination();
+  }
+}
